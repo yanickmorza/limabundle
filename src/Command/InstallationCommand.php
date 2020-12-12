5d8d30a09cf06c633a13d3bf473785a3867c9763
yanickmorza <yanick.morza@free.fr>
2020-12-12 19:55:46 +0100

    Limabundle-12-12-2020

--- src/Command/InstallationCommand.php
+++ src/Command/InstallationCommand.php
@@ -1,6 +1,49 @@
 <?php
 
-// **** Ecriture dans le fichier lima_bundle.yaml ****
+namespace App\LimaBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class InstallationCommand extends Command
+{
+    protected static $defaultName = 'lima:install';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Copie des fichiers twig dans le répertoire templates')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            copy('vendor/yanickmorza/limabundle/src/Command/_flashes.html.twig', 'templates/_flashes.html.twig');
+            copy('vendor/yanickmorza/limabundle/src/Command/base.html.twig', 'templates/base.html.twig');
+        }
+
+        $io->success('Copie réalisée avec succés !');
+
+        return Command::SUCCESS;
+    }
+}
+
+
+/* // **** Ecriture dans le fichier lima_bundle.yaml ****
 $path = "config/routes/dev/lima_bundle.yaml";
 fopen($path, "w+");
 
@@ -52,7 +95,7 @@ file_put_contents($path, $texte);
 copy('vendor/yanickmorza/limabundle/src/Command/_flashes.html.twig', 'templates/_flashes.html.twig');
 copy('vendor/yanickmorza/limabundle/src/Command/base.html.twig', 'templates/base.html.twig');
 // *** Copier les fichiers base.html.twig, _flashes.html.twig dans templates ***
-
+ */
 
 /*
 "scripts":{
