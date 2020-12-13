8ccf33769bc1f07e9c45f7787df24a631991ceec
yanickmorza <yanick.morza@free.fr>
2020-12-13 12:57:57 +0100

    Limabundle-13-12-2020

--- src/Command/Installation.php
+++ src/Command/Installation.php
@@ -1,66 +1,60 @@
 <?php
 
-// **** Ecriture dans le fichier lima_bundle.yaml ****
-$path = "config/routes/dev/lima_bundle.yaml";
-fopen($path, "w+");
+$filename = "config/routes/dev/lima_bundle.yaml";
 
-$texte = "app_file:
-    # Charger les routes à partir du fichier de routage stockée dans LimaBundle
-    resource: '@LimaBundle/Resources/config/routing_dev.yaml'
-    prefix: /lima";
+if (!file_exists($filename)) {
 
-file_put_contents($path, $texte);
-// **** Ecriture dans le fichier lima_bundle.yaml ****
+    // **** Ecriture dans le fichier lima_bundle.yaml ****
+    $path = "config/routes/dev/lima_bundle.yaml";
+    fopen($path, "w+");
 
-// **** Ecriture dans le fichier services.yaml ****
-$path = "config/services.yaml";
-fopen($path, "a");
+    $texte = "app_file:
+        # Charger les routes à partir du fichier de routage stockée dans LimaBundle
+        resource: '@LimaBundle/Resources/config/routing_dev.yaml'
+        prefix: /lima";
 
-$texte = "    App\LimaBundle\:
-        resource: '../vendor/yanickmorza/limabundle/src/'";
+    file_put_contents($path, $texte);
+    // **** Ecriture dans le fichier lima_bundle.yaml ****
 
-file_put_contents($path, PHP_EOL.$texte, FILE_APPEND);
-// **** Ecriture dans le fichier services.yaml ****
+    // **** Ecriture dans le fichier services.yaml ****
+    $path = "config/services.yaml";
+    fopen($path, "a");
 
-// ****** Ecriture dans le fichier twig.yaml ******
-$path = "config/packages/twig.yaml";
-fopen($path, "a");
+    $texte = "    App\LimaBundle\:
+            resource: '../vendor/yanickmorza/limabundle/src/'";
 
-$texte = "twig:
-    default_path: '%kernel.project_dir%/templates'
-    form_themes: ['bootstrap_4_layout.html.twig']";
+    file_put_contents($path, PHP_EOL.$texte, FILE_APPEND);
+    // **** Ecriture dans le fichier services.yaml ****
 
-file_put_contents($path, $texte);
-// ****** Ecriture dans le fichier twig.yaml ******
+    // ****** Ecriture dans le fichier twig.yaml ******
+    $path = "config/packages/twig.yaml";
+    fopen($path, "a");
 
-// *** Ecriture dans le fichier translation.yaml **
-$path = "config/packages/translation.yaml";
-fopen($path, "a");
+    $texte = "twig:
+        default_path: '%kernel.project_dir%/templates'
+        form_themes: ['bootstrap_4_layout.html.twig']";
 
-$texte = "framework:
-    default_locale: fr
-    translator:
-        enabled: false
-        default_path: '%kernel.project_dir%/translations'
-        fallbacks:
-            - fr";
+    file_put_contents($path, $texte);
+    // ****** Ecriture dans le fichier twig.yaml ******
 
-file_put_contents($path, $texte);
-// *** Ecriture dans le fichier translation.yaml **
+    // *** Ecriture dans le fichier translation.yaml **
+    $path = "config/packages/translation.yaml";
+    fopen($path, "a");
 
-// *** Copier les fichiers base.html.twig, _flashes.html.twig dans templates ***
-copy('vendor/yanickmorza/limabundle/src/Command/_flashes.html.twig', 'templates/_flashes.html.twig');
-copy('vendor/yanickmorza/limabundle/src/Command/base.html.twig', 'templates/base.html.twig');
-// *** Copier les fichiers base.html.twig, _flashes.html.twig dans templates ***
+    $texte = "framework:
+        default_locale: fr
+        translator:
+            enabled: false
+            default_path: '%kernel.project_dir%/translations'
+            fallbacks:
+                - fr";
 
+    file_put_contents($path, $texte);
+    // *** Ecriture dans le fichier translation.yaml **
 
-/*
-"scripts":{
-        "post-create-project-cmd": [
-            "php -r \"copy('config/example.php', 'config/local.php');\"",
-        ],
-        "execute": [
-            "@php vendor/yanickmorza/limabundle/src/command/installation.php"
-        ]
-},
-*/
+    // *** Copier les fichiers base.html.twig, _flashes.html.twig dans templates ***
+    copy('vendor/yanickmorza/limabundle/src/Command/_flashes.html.twig', 'templates/_flashes.html.twig');
+    copy('vendor/yanickmorza/limabundle/src/Command/base.html.twig', 'templates/base.html.twig');
+    // *** Copier les fichiers base.html.twig, _flashes.html.twig dans templates ***
+
+}
