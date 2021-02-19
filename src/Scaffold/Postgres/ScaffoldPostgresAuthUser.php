c640d8a9ee2ae4afd805182ece32ad201a3d49c5
yanickmorza <yanick.morza@free.fr>
2021-02-19 19:43:57 +0100

    Limabundle-19-02-2021

--- src/Scaffold/Postgres/ScaffoldPostgresAuthUser.php
+++ src/Scaffold/Postgres/ScaffoldPostgresAuthUser.php
@@ -792,6 +792,7 @@ namespace App\Form$nameSpace;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
