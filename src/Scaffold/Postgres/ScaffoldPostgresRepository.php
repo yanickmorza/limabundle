beb207b78a4a1dd324500565b6697fe7ea5463e0
yanickmorza <yanick.morza@free.fr>
2022-06-07 17:41:20 +0200

    Limabundle-07-06-2022

--- src/Scaffold/Postgres/ScaffoldPostgresRepository.php
+++ src/Scaffold/Postgres/ScaffoldPostgresRepository.php
@@ -134,6 +134,81 @@ class $ObjetRepository extends ServiceEntityRepository
             \$this->_em->flush();
         }
     }
+
+    // ------ Vider les donnees de la table -------
+    public function truncateTable()
+    {
+        \$stmt = \$this->getEntityManager()->getConnection()->prepare('TRUNCATE TABLE $objet CASCADE')->executeQuery();
+        \$stmt = \$this->getEntityManager()->getConnection()->prepare('ALTER SEQUENCE $sequence RESTART WITH 1')->executeQuery();       
+        return \$stmt; 
+    }
+    // ------ Vider les donnees de la table -------
+
+    // ----------- Exporter les donnees -----------
+    public function exporterDonnee$Objet()
+    {
+        \$stmt = \$this->getEntityManager()->getConnection()->prepare('SELECT * FROM $objet ORDER BY id ASC');
+        return \$stmt->executeQuery()->fetchAllAssociative();
+    }
+    // ----------- Exporter les donnees -----------
+
+    // ----------- Uploader les donnees -----------
+    public function uploaderDonnee$Objet()
+	{
+        \$tmp_file = \$_FILES['charger']['tmp_name'];
+        \$colonnes = \$this->getEntityManager()->getConnection()->getSchemaManager()->listTableColumns('sites');
+
+        \$parameter = '';
+        \$values = '';
+        \$datas = [];
+
+			foreach (\$colonnes as \$colonne) 
+            {
+                if (\$colonne->getName() != 'id') {
+                    \$values .= ':'.\$colonne->getName().', ';
+                    \$parameter .= \$colonne->getName().', ';
+                }
+            }
+
+        // Enlever la derniere virgule
+        \$values = substr(\$values, 0, -2);
+        \$parameter = substr(\$parameter, 0, -2);
+
+        \$tmp_file = \$_FILES['charger']['tmp_name'];
+        
+        if ((\$tmp_file) && (\$tmp_file != \"none\")) {
+
+            \$path_cache = \"../var/tmp/\".\$_FILES['charger']['name'];
+			move_uploaded_file (\$tmp_file, \$path_cache);
+
+			\$file = \$path_cache;
+			\$fichier = file_get_contents(\$path_cache);
+
+            \$newfile = trim(\$fichier);
+            file_put_contents(\$file, \$newfile);
+
+            \$handle = fopen(\$path_cache, \"r\");
+	
+				while ((\$data = fgetcsv(\$handle, 0, \";\")) !== FALSE) {
+					\$i = 0;
+					foreach (\$colonnes as \$colonne)
+            		{
+                		if (\$colonne->getName() != 'id') {
+							\$datas[\$colonne->getName()] = [\$colonne->getName() => \$data[\$i]][\$colonne->getName()];
+							\$i++;
+						}
+					}
+
+                    \$rawSql = \"INSERT INTO sites (\$parameter) VALUES (\$values)\";
+            		\$execution = \$this->getEntityManager()->getConnection()->prepare(\$rawSql)->executeQuery(\$datas);
+				}
+
+            fclose(\$handle);
+            unlink(\$path_cache);
+        }
+        return \$execution;
+    }
+    // ----------- Uploader les donnees -----------
 }
 ";
         file_put_contents($fichier_repository, $texte_repository);
